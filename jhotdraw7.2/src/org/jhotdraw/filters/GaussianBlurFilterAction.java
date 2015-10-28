ee16512b1fba602fb71dd87e0775877da06360a0
Kyzaq <kyzaq@github.com>
2015-10-28 10:01:13 +0100

    dont mind dis

--- jhotdraw7.2/src/org/jhotdraw/filters/GaussianBlurFilterAction.java
+++ jhotdraw7.2/src/org/jhotdraw/filters/GaussianBlurFilterAction.java
@@ -1,8 +1,16 @@
 package org.jhotdraw.filters;
 
 import java.awt.event.ActionEvent;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 import javax.swing.AbstractAction;
 import org.jhotdraw.app.Application;
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.Figure;
+import org.jhotdraw.draw.action.AbstractSelectedAction;
 import org.jhotdraw.util.ResourceBundleUtil;
 
 /**
@@ -12,6 +20,10 @@ import org.jhotdraw.util.ResourceBundleUtil;
 public class GaussianBlurFilterAction extends AbstractAction {
     public final static String ID = "filter.gaussianBlur";
 
+    /*public GaussianBlurFilterAction(DrawingEditor editor) {
+        super(editor);
+    }*/
+    
     public GaussianBlurFilterAction(Application app){
         ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.app.labels");
         labels.configureAction(this, ID);
@@ -21,5 +33,17 @@ public class GaussianBlurFilterAction extends AbstractAction {
         throw new UnsupportedOperationException("Not supported yet.");
     }
     
+    /*
+     * final DrawingView view = getView();
+        final LinkedList<Figure> figures = new LinkedList<Figure>(view.getSelectedFigures());
+    public static void bringToFront(DrawingView view, Collection<Figure> figures) {
+        Drawing drawing = view.getDrawing();
+        Iterator i = drawing.sort(figures).iterator();
+        while (i.hasNext()) {
+            Figure figure = (Figure) i.next();
+            drawing.bringToFront(figure);
+        }
+    }
+     */
     
 }
