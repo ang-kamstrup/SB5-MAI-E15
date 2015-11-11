fee91f63ddcad758a4b193317020f760980a9bc3
Jesper Wohlert <wohlert.jesper@gmail.com>
2015-11-11 09:48:43 +0100

    Added tests for ZoomSelection

---
+++ jhotdraw7.2/test/ZoomSelectionTest.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author jesper_wohlert
+ */
+public class ZoomSelectionTest {
+    
+    public ZoomSelectionTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+}
