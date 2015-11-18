0b2fd79142141033250e050cbeefa48a51ff3385
carentsen <dr.c.arentsen@gmail.com>
2015-11-18 22:04:42 +0100

    Kode
    Tilføjet kode til at painte image, men der mangler kode til at anvende
    funktionaliteten--

--- jhotdraw7.2/src/org/jhotdraw/draw/action/FeatherAction.java
+++ jhotdraw7.2/src/org/jhotdraw/draw/action/FeatherAction.java
@@ -14,6 +14,7 @@ import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.geom.Point2D;
 import org.jhotdraw.app.JHotDrawFeatures;
 import org.jhotdraw.draw.*;
 import org.jhotdraw.geom.*;
@@ -24,23 +25,79 @@ import static org.jhotdraw.draw.AttributeKeys.*;
  * @author ChristianArentsen
  */
 public class FeatherAction extends AbstractDrawingViewAction {
-    
-     private ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.draw.Labels", Locale.getDefault());
-     private DrawingView view = getView();
-     
-public FeatherAction(DrawingView view) {
+
+    private ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.draw.Labels", Locale.getDefault());
+    private DrawingView view = getView();
+    private DrawingEditor editor;
+    private java.awt.Graphics gr;
+    private java.awt.Component c;
+    private CreationTool ct;
+    private AttributeKey<Double> opacityKey;
+    private AttributeKey<Color> fillColorKey;
+    private AttributeKey<Color> strokeColorKey;
+    private Image image;
+    private Shape fillShape;
+    private Shape strokeShape;
+
+    public FeatherAction(DrawingView view) {
         super(view);
         labels.configureAction(this, "edit.feather");
         setEnabled(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-        if(!view.getSelectedFigures().isEmpty()){
-            
+        Graphics2D g = (Graphics2D) gr;
+        //paintIcon(c, g, x, y);
+
+
+    }
+
+    public void paintIcon(java.awt.Component c, java.awt.Graphics gr, int x, int y) {
+        Graphics2D g = (Graphics2D) gr;
+        paintIcon(c, g, x, y);
+        Double opacity;
+        Color fillColor;
+        Color strokeColor;
+        Figure createdFigure;
+        Point anchor = new Point();
+        DrawingView view = editor.getActiveView();
+        if (view != null && view.getSelectedFigures().size() == 1) {
+            opacity = opacityKey.get(view.getSelectedFigures().iterator().next());
+            fillColor = (fillColorKey == null) ? null : fillColorKey.get(view.getSelectedFigures().iterator().next());
+            strokeColor = (strokeColorKey == null) ? null : strokeColorKey.get(view.getSelectedFigures().iterator().next());
+        } else {
+            opacity = opacityKey.get(editor.getDefaultAttributes());
+            fillColor = (fillColorKey == null) ? null : fillColorKey.get(editor.getDefaultAttributes());
+            strokeColor = (strokeColorKey == null) ? null : strokeColorKey.get(editor.getDefaultAttributes());
         }
-        else{
-            
+
+        if (fillColorKey != null && fillShape != null) {
+            if (opacity != null) {
+                if (fillColor == null) {
+                    fillColor = Color.BLACK;
+                }
+                g.setColor(new Color((((int) (opacity * 255)) << 24) | (fillColor.getRGB() & 0xffffff), true));
+                g.translate(x, y);
+                g.fill(fillShape);
+                g.translate(-x, -y);
+//                createdFigure = ct.createFigure();
+//                Point2D.Double p = ct.constrainPoint(ct.viewToDrawing(anchor));
+                anchor.x = x;
+                anchor.y = y;
+//                createdFigure.setBounds(p, p);
+//                getDrawing().add(createdFigure);
+            }
+        }
+        if (strokeColorKey != null && strokeShape != null) {
+            if (opacity != null) {
+                if (strokeColor == null) {
+                    strokeColor = Color.BLACK;
+                }
+                g.setColor(new Color((((int) (opacity * 255)) << 24) | (strokeColor.getRGB() & 0xffffff), true));
+                g.translate(x, y);
+                g.draw(strokeShape);
+                g.translate(-x, -y);
+            }
         }
     }
 }
-
