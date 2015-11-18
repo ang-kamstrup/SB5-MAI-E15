370f900f15eaf93f7769548724f5a4f79c5763df
von Frank <frank@disko-fd.dk>
2015-11-18 02:06:28 +0100

    Updates

--- jhotdraw7.2/src/org/jhotdraw/draw/action/CropAction.java
+++ jhotdraw7.2/src/org/jhotdraw/draw/action/CropAction.java
@@ -6,11 +6,12 @@
 package org.jhotdraw.draw.action;
 
 import java.awt.event.ActionEvent;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JToggleButton;
+import static org.jhotdraw.draw.AttributeKeys.*;
 import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.gui.DrawingAttributeEditorHandler;
+import org.jhotdraw.gui.JAttributeTextField;
 
 /**
  *
@@ -18,15 +19,26 @@ import org.jhotdraw.draw.DrawingEditor;
  */
 public class CropAction extends AbstractSelectedAction{
 
+    private JAttributeTextField<Double> widthField, heightField;
+    private DrawingEditor editor;
+    
     public CropAction(DrawingEditor editor) {
         super(editor);
         setEnabled(true);
+        this.editor = editor;
+        widthField = new JAttributeTextField<Double>();
+        heightField = new JAttributeTextField<Double>();
+        widthField.setAttributeValue(200.00);
+        heightField.setAttributeValue(200.00);
     }
 
     public void actionPerformed(ActionEvent ae) {
         JToggleButton button = (JToggleButton) ae.getSource();
         if(button.isSelected()){
             JOptionPane.showMessageDialog(null, "To use crop tool, click keyboard arrows, to adjust canvas", "Crop tool", JOptionPane.INFORMATION_MESSAGE);
+//            Double cw = CANVAS_WIDTH.get(getDrawing());
+//            Double ch = CANVAS_HEIGHT.get(getDrawing());
+//            System.out.println("Width: " + cw + " Height: " + ch);
         } else{
             System.out.println("Deselected");
         }
