8dfbc302c22729b71a5ae6afbb0a4c857309f8ac
Jesper Wohlert <wohlert.jesper@gmail.com>
2015-11-18 10:40:00 +0100

    Added GeometricDrawingView decorator

---
+++ jhotdraw7.2/src/org/jhotdraw/draw/GeometricDrawingView.java
@@ -0,0 +1,42 @@
+package org.jhotdraw.draw;
+
+import java.awt.Point;
+import javax.swing.JViewport;
+
+/**
+ * Defines a decorated version of the DefaultDrawingView
+ * that enables extension of the class with geometry related
+ * functionality.
+ * 
+ * Pattern: Decorator
+ * Role: ConcreteDecorator
+ * Associates: DefaultDrawingView (Component)
+ * 
+ * @author jesper_wohlert
+ */
+public class GeometricDrawingView extends DefaultDrawingView {
+    
+    private DefaultDrawingView view;
+    
+    public GeometricDrawingView(DefaultDrawingView view) {
+        this.view = view;
+    }
+    
+    /**
+     * Moves the focus of the drawing view to a
+     * specific point.
+     * @param point 
+     */
+    public void moveView(Point point) {
+        
+        view.invalidate();
+        
+        if (view.getParent() != null) {
+            view.getParent().validate();
+            ((JViewport) view.getParent()).setViewPosition(point);
+        }
+
+        view.repaint();
+    }
+    
+}
