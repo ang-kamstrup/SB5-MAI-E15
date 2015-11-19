e9dd983c6e67c803c63ff6670430cfa57aa0f4f9
peter-desktop <peter.groenbaek@gmail.com>
2015-11-19 10:44:29 +0100

    Fixed an exception

--- jhotdraw7.2/src/org/jhotdraw/draw/GradientHandleKit.java
+++ jhotdraw7.2/src/org/jhotdraw/draw/GradientHandleKit.java
@@ -224,8 +224,10 @@ public class GradientHandleKit {
             double b = g.getCY() - g.getFY();
             double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
             
-            offsets[0] = c;
-            g.setStops(offsets, g.getStopColors(), g.getStopOpacities());
+            if(c < 1) {
+                offsets[0] = c;
+                g.setStops(offsets, g.getStopColors(), g.getStopOpacities());
+            }
             
             if (TRANSFORM.get(owner) != null) {
                 try {
@@ -295,8 +297,10 @@ public class GradientHandleKit {
             double b = g.getCY() - g.getFY();
             double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
             
-            offsets[0] = c;
-            g.setStops(offsets, g.getStopColors(), g.getStopOpacities());
+            if(c < 1) {
+                offsets[0] = c;
+                g.setStops(offsets, g.getStopColors(), g.getStopOpacities());
+            }
             
             if (TRANSFORM.get(owner) != null) {
                 try {
