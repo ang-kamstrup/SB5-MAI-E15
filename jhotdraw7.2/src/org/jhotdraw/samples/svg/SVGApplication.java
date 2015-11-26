051a1f56cafc9d4d0bfd3a08cc8361a839f3f94b
Emil Frisk <emilnygaardfrisk@gmail.com>
2015-11-26 22:09:02 +0100

    Added ViewRevisionHistoryAction to the View menu. Updated SVGApplication, SVGApplicationModel and RevisionView.

--- jhotdraw7.2/src/org/jhotdraw/samples/svg/SVGApplication.java
+++ jhotdraw7.2/src/org/jhotdraw/samples/svg/SVGApplication.java
@@ -4,41 +4,54 @@
  */
 package org.jhotdraw.samples.svg;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jhotdraw.app.DefaultSDIApplication;
 import org.jhotdraw.revisionhistory.RevisionController;
+import org.jhotdraw.revisionhistory.RevisionView;
 
 /**
  *
  * @author emilfrisk
  */
 public class SVGApplication extends DefaultSDIApplication {
-    
-    static RevisionController revisionController;
-    
-    public SVGApplication(){
-    }
 
-    @Override
-    public void init() {
-        super.init();
-        revisionController = new RevisionController();
-    }
-    
-    public void revisionHistory(){
-        new Thread(new Runnable() {
+	private static RevisionController revisionController;
 
-            public void run() {
-                try {
-                    Thread.sleep(30000);
-                } catch (InterruptedException ex) {
-                    //TODO: gør noget ved denne
-                }
-                SVGView svgView  = (SVGView) getActiveView();
-                revisionController.saveRevision(svgView.getDrawing());
-            }
-        }).start();
-    }
-    
+	public SVGApplication() {
+	}
+
+	@Override
+	public void init() {
+		super.init();
+
+		revisionController = new RevisionController();
+		startRevisionHistory();
+
+		/*
+		revisionView = new RevisionView();
+		revisionView.setup();
+		revisionView.setVisible(true);
+		revisionView.setRevisionController(revisionController);
+		 */
+	}
+
+	public void startRevisionHistory() {
+		new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException ex) {
+						//TODO: gør noget ved denne
+					}
+
+					SVGView svgView = (SVGView) getActiveView();
+					revisionController.saveRevision(svgView.getDrawing());
+				}
+			}
+		}).start();
+	}
+
+	public RevisionController getRevisionController(){
+		return revisionController;
+	}
 }
